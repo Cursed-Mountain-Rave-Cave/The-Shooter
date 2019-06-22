a8d07480a539a9accfb034242ecad570ee0d3271
Aleksey Semenov <alexsemenov1610@gmail.com>
2019-06-22 13:51:18 +0200

    remove useless classes

--- core/src/com/theshooter/Logic/Entity/Palm.java
+++
@@ -1,8 +0,0 @@
-package com.theshooter.Logic.Entity;
-
-import com.theshooter.Screen.Depth;
-
-public class Palm extends Entity  {
-    public Palm(int x, int y) { super(x, y, 30, 30, Depth.THINGS, false); }
-    public Palm(int x, int y, boolean passable) { super(x, y, 30, 30, Depth.THINGS, passable); }
-}
