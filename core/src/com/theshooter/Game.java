b0eb09e62836572f454d426d3550862aff7dc4f2
EZavdovev <jekaweek1@gmail.com>
2019-06-25 00:29:01 +0200

    nothing
    n

--- core/src/com/theshooter/Game.java
+++ core/src/com/theshooter/Game.java
@@ -72,7 +72,8 @@ public class Game extends com.badlogic.gdx.Game {
 //		entityController.load("test2");
 
 		setScreen(mainMenu);
-
+        GameLoader games = new GameLoader();
+        games.load("level3");
 		entityController.load("level1");
 
 		gameScreen.screenObjects = entityController.getScreenObjectArray();
